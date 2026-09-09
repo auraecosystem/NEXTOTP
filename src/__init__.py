99e57f172577733f1b99a503ac65a6e13f1f2c51
Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>
2026-09-09 04:40:41 +0100

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import os
 import platform
 import sys
-import dsa
-import hashes
 
 from cffi import FFI
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import dsa
